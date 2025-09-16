ee24044c13dfafd1bdc9bed3cc92c60e8f83756d
떠 닝 <83468686+ddunin605@users.noreply.github.com>
2025-09-16 16:21:15 +0900

    Create DnfScoreApplication.java

---
+++ DnfScoreApplication.java
@@ -0,0 +1,11 @@
+package com.dunin.dnfscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnfScoreApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(DnfScoreApplication.class, args);
+  }
+}
